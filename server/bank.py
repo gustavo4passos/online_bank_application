7bf8d7b003cae97ad8779c5ea6cedefc4f3e72da
gustavo4passos <gustavo4passos@gmail.com>
2019-10-01 01:05:07 -0300

    Adds Bank class

---
+++ server/bank.py
@@ -0,0 +1,23 @@
+import json
+
+class Bank:
+	def __init__(self, database_file):
+		return
+	
+	def get_balance(self, account_number):
+		return
+	
+	def withdraw(self, account_number, amount):
+		return
+	
+	def deposit(self, destination_acount, amount):
+		return
+	
+	def transfer(self, origin_account, destination_account, amount):
+		return
+	
+	def login(self, account, password):
+		return
+	
+	def create_acount(self, id, name, manager_account):
+		return
